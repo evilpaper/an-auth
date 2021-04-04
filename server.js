00a65d9fecb0438dc927f4681438eb285848107b
evilpaper <pellburt@gmail.com>
2021-04-04 08:08:48 +0200

    Check if user is authorized before showing dashboard

--- server.js
+++ server.js
@@ -78,14 +78,27 @@ app.post("/login", (req, res) => {
   });
 });
 
-app.get("/dashboard", (req, res) => {
-  res.render("dashboard");
+app.get("/dashboard", (req, res, next) => {
+  if (!(req.session && req.session.userId)) {
+    return res.redirect("/login");
+  }
+
+  User.findById(req.session.userId, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.redirect("/login");
+    }
+    res.render("dashboard");
+  });
 });
 
 // Tell Espress to start a web server on the port we defined earlier
 // so we can run it locally and play around with it.
 app.listen(port, () => {
-  console.log("");
-  console.log("\x1b[36m%s\x1b[0m", "The server is listening to port " + port);
-  console.log("");
+  console.log(
+    "\n" + "\x1b[36m%s\x1b[0m",
+    "The server is listening to port " + port + "\n"
+  );
 });
